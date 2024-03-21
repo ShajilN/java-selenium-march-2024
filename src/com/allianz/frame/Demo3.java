21dfa7abaa45edd314883f712620aac13bb61919
ShajilN <shajilnizarudeen@gmail.com>
2024-03-21 00:34:52 -0700

    alert, frame, iframe

---
+++ src/com/allianz/frame/Demo3.java
@@ -0,0 +1,34 @@
+package com.allianz.frame;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        driver.get("https://netbanking.hdfcbank.com/netbanking/");
+        
+        //switch to frame or iframe - by name or id
+        driver.switchTo().frame("login_page");
+        
+      //enter userid as jack123
+        driver.findElement(By.xpath("//input[contains(@name,'LoginUserId')]")).sendKeys("jack123");
+        
+        driver.findElement(By.linkText("CONTINUE")).click();
+        
+        //switch to main html
+        driver.switchTo().defaultContent();
+
+	}
+
+}
